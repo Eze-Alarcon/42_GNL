568578b8f74fb3a220159b1928aa678a5ed7109c
ezequiel <ps3ezequiel@gmail.com>
2023-10-20 17:02:50 +0200

    modify text file and implement malloc

--- get_next_line.c
+++ get_next_line.c
@@ -8,6 +8,8 @@ char *get_next_line(int fd)
 	printf("buffer = %i\n", BUFFER_SIZE);
 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	chars = read(fd, buffer, BUFFER_SIZE);
+	*(buffer + chars + 1) = '\0';
 	printf("buffer: %s\n chars = %lu", buffer, chars);
+	free(buffer);
 	return (buffer);
 }
